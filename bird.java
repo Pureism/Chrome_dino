a1c369dbbc5abbb6537faeeab6baadc5c64c551d
Pureism <pureismx@gmail.com>
2021-07-04 00:09:20 +0700

    add obstacle java

---
+++ bird.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bird here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bird extends obstacle
+{
+    /**
+     * Act - do whatever the bird wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
