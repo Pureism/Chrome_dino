525a1f7343f0f313c5b536b03a758fe29babf55b
Pureism <pureismx@gmail.com>
2021-07-03 08:32:28 +0700

    initial commit

---
+++ MyWorld.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Ini adalah world class.
+ * @author (Muhammad Ammar Nabil) 
+ */
+public class MyWorld extends World
+{
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 300, 1); 
+    }
+}
